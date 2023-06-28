6523424a79b8bdf7f34cf66b8c92428594523974
pdebeaumont <philippe.debeaumont@outlook.fr>
2023-06-28 16:13:48 +0200

    Added incremental load

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -8,6 +8,8 @@ def spark_submit(event, context):
     dag = os.getenv("DAG_PATH")
     dataset = os.getenv("DATASET")
 
+    file = event["attributes"]["objectId"]
+
     job_client = dataproc.JobControllerClient(client_options={'api_endpoint': '{}-dataproc.googleapis.com:443'.format(region)})
     job = {
         'placement': {
@@ -16,7 +18,7 @@ def spark_submit(event, context):
         'pyspark_job': {
             'main_python_file_uri': f'gs://{bucket}/{dag}',
             'jar_file_uris': ['gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar'],
-            'args': ['--bucket', bucket, '--input_folder', f'gs://{bucket}/hires','--station_data', f'gs://{bucket}/london_cycle_stations.csv', 
+            'args': ['--bucket', bucket, '--input_file', f'gs://{bucket}/{file}','--station_data', f'gs://{bucket}/london_cycle_stations.csv', 
                      '--project', project_id, '--table_hires', f'{dataset}.cycle_hires', '--table_daily_agg', f'{dataset}.daily_agg']
         }
     }
